701e5a2e97850b84a7c61a06589a05723ecfe16c
PaulRichez <paul.richez59@gmail.com>
2022-10-12 04:44:46 +0200

    fix guard

--- EG2_client/src/app/core/guard/is-logged.guard.ts
+++ EG2_client/src/app/core/guard/is-logged.guard.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { map, Observable } from 'rxjs';
-import { ApiAuthService } from '../services/api-auth.service';
+import { AuthentificationService } from '../authentification/authentification.service';
 import { TokenStorageService } from '../services/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsLoggedGuard implements CanActivate {
-  constructor(public router: Router, private authService: ApiAuthService, private tokenStorageService: TokenStorageService) { }
+  constructor(public router: Router, private authService: AuthentificationService, private tokenStorageService: TokenStorageService) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
