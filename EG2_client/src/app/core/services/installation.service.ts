500d81966cc16edbfc6c1dfb0805974fb80122f2
PaulRichez <paul.richez59@gmail.com>
2022-10-16 08:38:58 +0200

    form

--- EG2_client/src/app/core/services/installation.service.ts
+++ EG2_client/src/app/core/services/installation.service.ts
@@ -1,16 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { requiredFileType } from '../validators/requiredFileType';
+import { requiredNumberType } from '../validators/requireNumberType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstallationService {
   public isFirstInstall = true;
+  public formWebsite!: FormGroup;
+  public formEmail!: FormGroup;
+  public formFirstUser!: FormGroup;
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+  ) {
+    this.setForm();
+  }
 
   public checkFirstInstall() {
     if (!this.isFirstInstall) {
@@ -22,4 +31,24 @@ export class InstallationService {
       }));
     }
   }
+
+  private setForm() {
+    this.formWebsite = this.formBuilder.group({
+      theme: ['', [Validators.required]],
+      society: ['', []],
+      logo: ['', [requiredFileType('png')]],
+    });
+    this.formEmail = this.formBuilder.group({
+      host: ['', [Validators.required]],
+      port: ['', [Validators.required, requiredNumberType()]],
+    })
+    this.formFirstUser = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
 }
