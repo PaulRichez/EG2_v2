0da77a80be07170b17c1ab7e65fafd7b7a5c1edc
PaulRichez <paul.richez59@gmail.com>
2022-10-18 06:59:27 +0200

    Client => add dashboard and admin

--- EG2_client/src/app/modules/home/home-routing.module.ts
+++ EG2_client/src/app/modules/home/home-routing.module.ts
@@ -4,7 +4,13 @@ import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent
+    path: '', component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
+      { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+
+    ]
   }
 ];
 
