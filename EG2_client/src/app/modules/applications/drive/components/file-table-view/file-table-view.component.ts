82456cb19a3be9a4313121dd1dfd6c6946861148
PaulRichez <paul.richez59@gmail.com>
2022-10-27 07:10:59 +0200

    fodler anvigation

--- EG2_client/src/app/modules/applications/drive/components/file-table-view/file-table-view.component.ts
+++ EG2_client/src/app/modules/applications/drive/components/file-table-view/file-table-view.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DriveService } from 'src/app/core/services/drive.service';
 import { AppHelperComponent } from 'src/app/shared/extends/app-helper/app-helper.component';
-import * as qs from 'qs'
+
 import { IFolder } from 'src/app/shared/models/folder.model';
 import { FilesHelperService } from 'src/app/core/services/files-helper.service';
 @Component({
@@ -12,10 +12,10 @@ import { FilesHelperService } from 'src/app/core/services/files-helper.service';
 })
 export class FileTableViewComponent extends AppHelperComponent implements OnInit, OnChanges {
   @Input() selectedFolderId!: string;
-  loadingData = true;
-  folder!: IFolder;
-  directoryData!: any[];
-  tableSelectedEntry: any;
+  @Input() loadingData = true;
+  @Input() folder!: IFolder;
+  @Output() dbClick = new EventEmitter<any>();
+  directoryData!: IFolder[] | any[];
   constructor(
     public override route: ActivatedRoute,
     private router: Router,
@@ -29,30 +29,16 @@ export class FileTableViewComponent extends AppHelperComponent implements OnInit
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const query = qs.stringify({
-      populate: ['deep'],
-    }, {
-      encodeValuesOnly: true,
-    });
-    this.loadingData = true;
-    this.driveService.findOne(this.selectedFolderId, query).subscribe({
-      next: result => {
-        this.folder = result;
-        this.directoryData = [];
-        if (this.folder.children) {
-          (this.folder.children as IFolder[]).forEach(f => this.directoryData.push(f))
-        }
-        if (this.folder.files) {
-          (this.folder.files as any[]).forEach(f => this.directoryData.push(f))
-        }
-        console.log(result)
-        this.loadingData = false;
-      },
-      error: err => {
-        this.loadingData = false;
-        console.error(err);
-      },
-    })
+    if (changes['folder']) {
+      this.directoryData = [];
+      if (this.folder?.children) {
+        (this.folder.children as IFolder[]).forEach(f => this.directoryData.push(f))
+      }
+      if (this.folder?.files) {
+        (this.folder.files as any[]).forEach(f => this.directoryData.push(f))
+      }
+    }
   }
+
 
 }
--- EG2_client/src/app/modules/applications/drive/pages/drive-main/drive-main.component.ts
+++ EG2_client/src/app/modules/applications/drive/pages/drive-main/drive-main.component.ts
@@ -3,7 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { DriveService } from 'src/app/core/services/drive.service';
 import { AppHelperComponent } from 'src/app/shared/extends/app-helper/app-helper.component';
-
+import * as qs from 'qs'
+import { IFolder } from 'src/app/shared/models/folder.model';
 @Component({
   selector: 'app-drive-main',
   templateUrl: './drive-main.component.html',
@@ -13,8 +14,10 @@ export class DriveMainComponent extends AppHelperComponent implements OnInit {
   items!: MenuItem[];
 
   home!: MenuItem;
+  public loadingData = true;
 
   public idFolder!: string
+  folder!: IFolder;
   constructor(
     public override route: ActivatedRoute,
     private router: Router,
@@ -34,11 +37,40 @@ export class DriveMainComponent extends AppHelperComponent implements OnInit {
           console.error(err)
         },
       })
+    } else {
+      this.fetchData()
     }
   }
 
+  fetchData() {
+    const query = qs.stringify({
+      populate: ['deep'],
+    }, {
+      encodeValuesOnly: true,
+    });
+    this.loadingData = true;
+    this.driveService.findOne(this.idFolder, query).subscribe({
+      next: result => {
+        this.folder = result;
+        this.loadingData = false;
+      },
+      error: err => {
+        this.loadingData = false;
+        console.error(err);
+      },
+    })
+  }
+
   goToFolder(id: string) {
+    this.idFolder = id;
     this.router.navigate([{ outlets: { ['primary']: '', [this.outlet as string]: ['tab', 'drive', id] } }])
+  }
+
+  dbClick(entry: IFolder | any) {
+    if (!entry.url) {
+      this.goToFolder(entry.id);
+      this.fetchData();
+    }
   }
 
 }
