3ca2a3080ef0ddc2ca787253bec45a91d3a38773
PaulRichez <paul.richez59@gmail.com>
2022-11-04 16:10:25 +0100

    calendar api

--- EG2_server/src/api/calendar/routes/event-source.js
+++ EG2_server/src/api/calendar/routes/event-source.js
@@ -6,4 +6,32 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::calendar.event-source');
+module.exports = createCoreRouter('api::calendar.event-source', {
+    config: {
+        create: {
+            roles: ["authenticated"],
+            middlewares: [
+                (ctx, next) => {
+                    const data = JSON.parse(ctx.request.body.data)
+                    if (!data.owner || data.owner.toString() !== ctx.state.user.id.toString()) {
+                        return ctx.badRequest(`Field owner error`);
+                    }
+                    return next();
+                },
+            ]
+        },
+        find: {
+            roles: ["authenticated"],
+        },
+        findOne: {
+            roles: ["authenticated"]
+        },
+        update: {
+            roles: ["authenticated"]
+        },
+        delete: {
+            roles: ["authenticated"]
+        }
+    }
+});
+
