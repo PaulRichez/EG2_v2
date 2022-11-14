2f0af6d58bd1c6b3713d1d55e63201e8dbb503a1
PaulRichez <paul.richez59@gmail.com>
2022-11-15 00:54:12 +0100

    news dashboard

--- EG2_client/src/app/modules/applications/dashboard/dashboard-routing.module.ts
+++ EG2_client/src/app/modules/applications/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardMainComponent } from './pages/dashboard-main/dashboard-main.component';
+import { NewComponent } from './pages/new/new.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  {
+    path: '', component: DashboardComponent, children: [
+      { path: '', component: DashboardMainComponent },
+      { path: 'new/:id', component: NewComponent },
+    ]
+  }
 ];
 
 @NgModule({
--- EG2_client/src/app/modules/applications/dashboard/dashboard.module.ts
+++ EG2_client/src/app/modules/applications/dashboard/dashboard.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardMainComponent } from './pages/dashboard-main/dashboard-main.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NewComponent } from './pages/new/new.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardMainComponent,
+    NewComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }
