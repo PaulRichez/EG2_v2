1073c6d19ee6318905a5db01d57685b9667dea8c
PaulRichez <paul.richez59@gmail.com>
2022-11-15 00:30:43 +0100

    retrait cl

--- EG2_server/src/plugins/emailengine/server/controllers/message.js
+++ EG2_server/src/plugins/emailengine/server/controllers/message.js
@@ -3,7 +3,6 @@
 module.exports = {
     async search(ctx, next) {
         const result = await strapi.service('plugin::emailengine.message').search(ctx.state.user.id, ctx.request.body.data, ctx.request.query);
-        console.log(result)
         ctx.status = result?.status || result?.statusCode || 200;
         ctx.body = result
     },
