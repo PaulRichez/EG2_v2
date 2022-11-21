2f790c5f577ee80719cb267e398a818b06cc360f
PaulRichez <paul.richez59@gmail.com>
2022-11-21 02:52:47 +0100

    demo message error

--- EG2_client/src/app/core/interceptor/http-interceptor.interceptor.ts
+++ EG2_client/src/app/core/interceptor/http-interceptor.interceptor.ts
@@ -1,12 +1,17 @@
-import { HttpEvent, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { TokenStorageService } from '../services/token-storage.service';
 import { map, Observable } from 'rxjs';
+import { tap } from "rxjs/operators";
+import { ToastrService } from 'ngx-toastr';
 const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(
+    private token: TokenStorageService,
+    private toastr: ToastrService
+  ) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.token.getToken();
     if (token != null) {
@@ -14,11 +19,20 @@ export class InterceptorService implements HttpInterceptor {
     }
     request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
     return next.handle(request).pipe(
-      map((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse) {
-          // console.log('event--->>>', event);
+      tap(
+        event => {
+          //logging the http response to browser's console in case of a success
+          if (event instanceof HttpResponse) {
+            // console.log("api call success :", event);
+          }
+        },
+        error => {
+          //logging the http response to browser's console in case of a failuer
+          if (error instanceof HttpErrorResponse && error.status === 403) {
+            this.toastr.error("Cette action n'est pas permise en mode démo")
+          }
         }
-        return event;
-      }));
+      )
+    );
   }
 }
--- EG2_server/src/plugins/emailengine/server/routes/account.js
+++ EG2_server/src/plugins/emailengine/server/routes/account.js
@@ -24,7 +24,7 @@ module.exports = {
             path: '/countAccounts',
             handler: 'account.countAccounts',
             config: {
-                roles: ['authenticated']
+                roles: ['authenticated', "demo"]
             }
         },
     ]
