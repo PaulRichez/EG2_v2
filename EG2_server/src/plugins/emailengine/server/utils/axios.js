8b49658ab80b1f411a5250743d1a62b02ba53044
PaulRichez <paul.richez59@gmail.com>
2022-11-21 03:08:47 +0100

    force demarage emailengine

--- EG2_server/src/plugins/emailengine/server/utils/axios.js
+++ EG2_server/src/plugins/emailengine/server/utils/axios.js
@@ -1,12 +1,12 @@
 module.exports = {
     getEmailengineUrl: function getEmailengineUrl(strapi, endpoint) {
         const config = strapi.config.get('plugin.emailengine')
-        const domain = config.domain || "http://localhost:3000"
+        const domain = config?.domain || "http://localhost:3000"
         return `${domain}/v1/${endpoint}`;
     },
     getEmailengineToken: async function getEmailengineToken(strapi) {
         const config = strapi.config.get('plugin.emailengine')
-        const token = config.token
+        const token = config?.token
         if (token) {
             return {
                 headers: { Authorization: `Bearer ${token}` }
